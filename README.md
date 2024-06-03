7f4705497f98850715c0b8121644be72f33eb7a2
Fax <fabrizio.rapelli@dovetail.ie>
2024-06-03 23:39:32 +0200

    Fix emscripten build

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# Template
+# Raylib c++ template project
+
+A very SIMPLE project that contains everything needed to build something.
+
+
+# Assets
+
+Background copied from https://swapnilrane24.itch.io/night-background-2
--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,30 @@
+#ifndef ASSETS_PATH
+#define ASSETS_PATH
+#endif
+
 #include <iostream>
 #include "raylib.h"
 
 int main(void)
 {
-    std::cout << "Init" << std::endl;
     InitWindow(800, 600, "Title");
     SetTargetFPS(60);
+
     Camera2D mainCamera;
     mainCamera.zoom = 1;
     mainCamera.offset = {(float)(GetScreenWidth() / 2), (float)(GetScreenHeight() / 2)};
     mainCamera.rotation = 0;
     mainCamera.target = {0, 0};
+
+    Texture2D background = LoadTexture(ASSETS_PATH "Flat Night 4 BG.png");
+
     while (!WindowShouldClose())
     {
         BeginDrawing();
         ClearBackground(LIGHTGRAY);
+        DrawTexture(background, 0, 0, WHITE);
         BeginMode2D(mainCamera);
-        DrawText("This is a new window", 0, 0, 10, RED);
+        DrawText("This is a new window", 0, 0, 10, WHITE);
         EndMode2D();
         EndDrawing();
     }
